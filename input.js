d42fed2d3a42f1cf4be27abaea2b35c88cb4bc24
Peter Hashem <peterhashem@gmail.com>
2023-09-20 19:49:08 -0400

    added special keys for canned messages

--- input.js
+++ input.js
@@ -1,10 +1,12 @@
 let connection;
 
 const handleUserInput = function (key, conn) {
+  //special exit key
   if (key === "\u0003") {
     process.exit();
   }
 
+  //check for movement keys and corresponding commands
   if (key === "w") {
     conn.write("Move: up");
   } else if (key === "a") {
@@ -13,6 +15,13 @@ const handleUserInput = function (key, conn) {
     conn.write("Move: down");
   } else if (key === "d") {
     conn.write("Move: right");
+  }
+
+  //Implement special keys for canned messages
+  if (key === "1") {
+    conn.write("Say: Greetings!");
+  } else if (key === "2") {
+    conn.write("Say: Let's Slither!");
   }
 };
 
