3140e668787abcbe2d217a6f5a5b723ee508b967
Peter Hashem <peterhashem@gmail.com>
2023-09-20 19:06:55 -0400

    refactored functions to handle key presses

--- input.js
+++ input.js
@@ -1,15 +1,28 @@
-const handleUserInput = function (key) {
+let connection;
+
+const handleUserInput = function (key, conn) {
   if (key === "\u0003") {
     process.exit();
   }
+
+  if (key === "w") {
+    conn.write("Move: up");
+  } else if (key === "a") {
+    conn.write("Move: left");
+  } else if (key === "s") {
+    conn.write("Move: down");
+  } else if (key === "d") {
+    conn.write("Move: right");
+  }
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => handleUserInput(key, conn));
   return stdin;
 };
 
